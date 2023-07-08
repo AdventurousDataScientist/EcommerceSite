fc6ea18e423604c50c436adcf349c8965c1a8cef
Rollingterminator1 <ngaur20@gmail.com>
2023-07-08 18:52:50 -0500

    updated populate inventory to include image

--- ecommerce_site/populate_inventory.py
+++ ecommerce_site/populate_inventory.py
@@ -6,6 +6,7 @@ import pandas as pd
 products_json_data_df = pd.read_csv('FAKE_STORE_PRODUCTS.csv')
 for index, row in products_json_data_df.iterrows():
     name = row["name"]
+    image = row["image"]
     price = row["price"]
     description = row["description"]
     stock = row["stock"]
@@ -13,7 +14,7 @@ for index, row in products_json_data_df.iterrows():
     category = row["category"]
     main_user = User.objects.get(username='nikhil') # should change this to test_user_1
     store = main_user.store_set.first()
-    item = Item(store=store, name=name, price=price, description=description, stock=stock, rating=rating, category=category)
+    item = Item(store=store, name=name, image=image, price=price, description=description, stock=stock, rating=rating, category=category)
     item.save()
 
 
